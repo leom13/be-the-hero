021fc94f38f8ea335742c20cea0d0b86317c24e3
leom13 <leonardo.mesquita1307@gmail.com>
2022-09-17 18:19:12 -0300

    Back-end da aplicacao

---
+++ backend/src/database/connection.js
@@ -0,0 +1,6 @@
+const { default: knex } = require("knex");
+const configuration = require('../../knexfile');
+
+const connection = knex(configuration.development);
+
+module.exports = connection;
---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+
+const routes = require('./routes');
+
+const app = express();
+
+const cors = require('cors');
+
+/**
+ * precisamos informar para o app qie iremos usar o .JSON, usando o código abaixo
+*/
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
